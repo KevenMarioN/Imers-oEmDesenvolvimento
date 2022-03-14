851327b733bf56dcf117665b2d6c522a9b0f8ed8
KevenMarioN <kevenmario.n.r@gmail.com>
2022-03-14 08:39:53 -0300

    feat: desmotração do uso de eventos personalizados

---
+++ 03-events/index.js
@@ -0,0 +1,36 @@
+const EventEmitter = require('events');
+class MeuEmissor extends EventEmitter {
+
+}
+
+const meuEmissor = new MeuEmissor();
+const nomeEvento = 'usuario:click';
+
+meuEmissor.on(nomeEvento, function (click) {
+  console.log('Um usuario clicou,', click);
+});
+
+// meuEmissor.emit(nomeEvento,'na barra de rolagem');
+// meuEmissor.emit(nomeEvento,'na ok');
+// let count = 0
+// setInterval(function(){
+//   meuEmissor.emit(nomeEvento,'na ok' + (count++));
+// },1000);
+
+const stdin = process.openStdin();
+stdin.addListener('data', function (value) {
+  console.log(`Você digitou : ${value.toString().trim()}`);
+});
+
+// Desta forma só executa uma unica veaz
+// function main() {
+//   return new Promise(function (resolve,reject){
+//     stdin.addListener('data',function(value) {
+//       // console.log(`Você digitou : ${value.toString().trim()}`);
+//       return resolve(value)
+//     });
+//   });
+// }
+// main().then(function(resultado) {
+//   console.log('resultado',resultado.toString());
+// });
