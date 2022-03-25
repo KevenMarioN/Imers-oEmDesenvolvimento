ef804e3fd3a79ef93a5096cdff3bed565042cf24
KevenMarioN <kevenmario.n.r@gmail.com>
2022-03-25 17:54:43 -0300

    hapi e legal

---
+++ 15-hapi-listar-joi/src/routes/heroiRoutes.js
@@ -0,0 +1,40 @@
+const BaseRoute = require("./base/baseRoute");
+const Joi = require('joi');
+class HeroRoute extends BaseRoute {
+  constructor(db) {
+    super();
+    this.db = db
+  }
+  list() {
+    return {
+      path: '/herois',
+      method: 'GET',
+      config: {
+        validate: {
+          failAction: (request, headers, erro) => {
+            throw erro
+          },
+          query: Joi.object({
+            skip: Joi.number().integer().default(0),
+            limit: Joi.number().integer().default(10),
+            nome: Joi.string().min(3).max(100).default('')
+          })
+        }
+      },
+      handler: (request, head) => {
+        try {
+          const { nome, skip, limit } = request.query;
+          const query = { 
+            nome: { 
+              $regex: `.*${nome || ''}*.`
+             } };
+
+          return this.db.read(query, parseInt(skip), parseInt(limit));
+        } catch (error) {
+          return `ERROR`;
+        }
+      }
+    }
+  }
+}
+module.exports = HeroRoute;
---
+++ 15-hapi-listar-joi/src/tests/apiHeros.test.js
@@ -0,0 +1,39 @@
+const { deepEqual, ok } = require('assert');
+const api = require('../api');
+let app = {};
+describe.only('Teste for API HEROES', function () {
+  this.beforeAll(async function () {
+    app = await api
+  });
+  it('Should be able render list', async () => {
+    const result = await app.inject({
+      method: 'GET',
+      url: '/herois'
+    });
+
+    const { statusCode, payload } = result;
+    const data = JSON.parse(payload);
+    deepEqual(statusCode, 200);
+    ok(Array.isArray(data));
+  });
+  it('Should be able render list with limit', async () => {
+    const LIMIT_ITEM = 3;
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&limit=${LIMIT_ITEM}`
+    });
+
+    const { statusCode, payload } = result;
+    const data = JSON.parse(payload);
+    deepEqual(statusCode, 200);
+    ok(data.length === LIMIT_ITEM);
+  });
+  it('Should be able render error with limit', async () => {
+    const LIMIT_ITEM = "kajdkshadj";
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&limit=${LIMIT_ITEM}`
+    });
+    deepEqual(result.statusCode, 400);
+  });
+});
