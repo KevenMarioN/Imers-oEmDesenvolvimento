28db12cb5cae713acd9e7e9659074115a7108ba6
KevenMarioN <kevenmario.n.r@gmail.com>
2022-03-28 10:05:56 -0300

    create route for patch of data

---
+++ 17-hapi-atualizar/src/tests/apiHeros.test.js
@@ -0,0 +1,106 @@
+const { deepEqual, ok, notStrictEqual } = require('assert');
+const { assert } = require('console');
+const { isStringLiteral } = require('typescript');
+const api = require('../api');
+let app = {};
+const MOCK_HERO_CREATE = {
+  nome: `Luffy-${Date.now()}`,
+  poder: 'Nika Nika no mi'
+}
+const MOCK_HERO_STARTED = {
+  nome: `Franklin-${Date.now()}`,
+  poder: 'CYBORG'
+}
+let MOCK_ID = '';
+describe.only('Teste for API HEROES', function () {
+  this.beforeAll(async function () {
+    app = await api
+    const { payload } = await app.inject({
+      url: '/herois',
+      method: 'POST',
+      payload: MOCK_HERO_STARTED
+    });
+    const { _id } = JSON.parse(payload);
+    MOCK_ID = _id;
+  });
+  it('Should be able render list', async () => {
+    const result = await app.inject({
+      method: 'GET',
+      url: '/herois?skip=0&limit=10'
+    });
+
+    const { statusCode, payload } = result;
+    const data = JSON.parse(payload);
+    deepEqual(statusCode, 200);
+    ok(Array.isArray(data));
+  });
+  it('Should be able render list with limit', async () => {
+    const LIMIT_ITEM = 3;
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&limit=${LIMIT_ITEM}`
+    });
+
+    const { statusCode, payload } = result;
+    const data = JSON.parse(payload);
+    deepEqual(statusCode, 200);
+    ok(data.length === LIMIT_ITEM);
+  });
+  it('Should be able render error with limit', async () => {
+    const LIMIT_ITEM = "kajdkshadj";
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&limit=${LIMIT_ITEM}`
+    });
+    deepEqual(result.statusCode, 400);
+  });
+  it('Should be able create new hero', async () => {
+    const { statusCode, payload } = await app.inject({
+      method: 'POST',
+      url: '/herois',
+      payload: MOCK_HERO_CREATE
+    });
+    const { message, _id } = JSON.parse(payload);
+
+    notStrictEqual(_id, undefined);
+    deepEqual(message, "Heroi cadastrado com sucesso!");
+    deepEqual(statusCode, 200);
+  });
+  it('Should be able return erro in method POST, if not exists nome or poder', async () => {
+    const { statusCode } = await app.inject({
+      method: 'POST',
+      url: '/herois'
+    });
+    deepEqual(statusCode, 400);
+  });
+  it('Should be able actualization a hero', async () => {
+    const _id = MOCK_ID;
+    const expected = {
+      poder: 'Super Mira'
+    }
+    const result = await app.inject({
+      method: 'PATCH',
+      url: `/herois/${_id}`,
+      payload: expected
+    });
+    const { statusCode, payload } = result;
+    ok(statusCode === 200);
+    const data = JSON.parse(payload);
+    deepEqual(data.message, 'Heroi atualizado com sucesso!');
+  });
+  it('Should be able try actualization a hero', async () => {
+    const _id = `6241b1414d86c30d3c229434`;
+    const expected = {
+      poder: 'Super Mira'
+    }
+    const result = await app.inject({
+      method: 'PATCH',
+      url: `/herois/${_id}`,
+      payload: expected
+    });
+    const { statusCode, payload } = result;
+    ok(statusCode === 200);
+    const data = JSON.parse(payload)
+    deepEqual(data.message, 'Não foi possível atualizar');
+  });
+});
